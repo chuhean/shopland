d8ec65eab258fee9d1c43f7cbc0ec26ed51fd1ba
Chu Hean <chuhean@gmail.com>
2018-04-12 12:58:07 +0000

    add navbar and carousel

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+1. Design website
+2. Set up component views
+3. Set up RESTful routes
+4. Set up models and authetication
+5. Optimization, cleaning up, write comment
+6. Testing: functionality, DB, security, performance (front & back, DB), cross-browser 
+7. Documentation & maintenance 
+
+*git add, commit and push to github
+*HTML, CSS, JS, NodeJS, MongoDB, ExpressJS, Mongoose, Angular
+
 # Shopland
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.4.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    CarouselComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
